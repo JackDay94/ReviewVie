f853fb5b2fa0f2ef3ebac87abcabf329aa133824
JackDay94 <Jack94day@gmail.com>
2022-12-14 22:34:18 +0000

    Create the Profile model to store user profile information

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    """
+    Profile model
+    """
+    class Genre(models.TextChoices):
+        """
+        Choices for the Genre field
+        """
+        NP = 'NP', 'No Preference'
+        ACT = 'ACT', 'Action'
+        ADV = 'ADV', 'Adventure'
+        COM = 'COM', 'Comedy'
+        CRI = 'CRI', 'Crime'
+        DRA = 'DRA', 'Drama'
+        FAM = 'FAM', 'Family'
+        FAN = 'FAN', 'Fantasy'
+        HOR = 'HOR', 'Horror'
+        MUS = 'MUS', 'Music'
+        ROM = 'ROM', 'Romance'
+        SCI = 'SCI', 'Sci-Fi'
+        SPT = 'SPT', 'Sport'
+        SUP = 'SUP', 'Superhero'
+        THR = 'THR', 'Thriller'
+        WAR = 'WAR', 'War'
+        WST = 'WST', 'Western'
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    favourite_genre = models.CharField(choices=Genre.choices, max_length=50)
+    favourite_director = models.CharField(max_length=50)
+    age = models.IntegerField()
+
+    def __str__(self):
+        return f"Profile of {self.user}"
