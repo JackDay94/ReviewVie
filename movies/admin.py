5307dbc9cb686bd193c4fe09dd7ad3ea18680d8b
JackDay94 <Jack94day@gmail.com>
2022-12-16 23:09:06 +0000

    Create and add Review model for reviews on movies and add to admin panel

--- movies/admin.py
+++ movies/admin.py
@@ -1,24 +1,45 @@
 from django.contrib import admin
-from .models import Movies
+from .models import Movie, Review
 
 
-@admin.register(Movies)
-class MoviesAdmin(admin.ModelAdmin):
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
     list_display = (
         'name',
         'genre',
         'director',
         'release_date',
         'age_rating',
-        'average_stars'
+        'average_stars',
+        'approved'
         )
     list_filter = (
         'genre',
         'director',
         'age_rating',
-        'average_stars'
+        'average_stars',
+        'approved'
         )
     search_fields = [
+        'name',
         'genre',
         'director',
         ]
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = (
+        'author',
+        'movie',
+        'rating',
+        'created_on'
+    )
+    list_filter = (
+        'created_on',
+        'movie__name'
+    )
+    search_fields = [
+        'author',
+        'movie',
+    ]
--- movies/models.py
+++ movies/models.py
@@ -4,7 +4,7 @@ from profiles.models import Profile
 from cloudinary.models import CloudinaryField
 
 
-class Movies(models.Model):
+class Movie(models.Model):
 
     name = models.CharField(max_length=100, unique=True)
     slug = models.SlugField(max_length=100, unique=True, null=True)
@@ -16,8 +16,20 @@ class Movies(models.Model):
     age_rating = models.IntegerField()
     summary = models.TextField(max_length=1000)
     image = CloudinaryField("movie image", default="placeholder_image")
-    average_stars = models.FloatField()
+    average_stars = models.FloatField(default=0)
     approved = models.BooleanField(default=False)
 
     def __str__(self):
         return f"{self.name}"
+
+
+class Review(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    review_content = models.TextField(max_length=1000)
+    rating = models.FloatField(default=0)
+    created_on = models.DateTimeField(auto_now_add=True)
+    edited_on = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Review of {self.movie} by {self.author}"
