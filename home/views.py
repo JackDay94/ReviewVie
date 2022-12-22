d7ee7dfb0721574cb09f4435d776269c2735964e
JackDay94 <Jack94day@gmail.com>
2022-12-22 15:59:03 +0000

    Add basic layout to Latest Reviews section on home page and adjust movie img styles

--- home/views.py
+++ home/views.py
@@ -8,14 +8,13 @@ class MoviesHome(ListView):
     model = Movie, Review
     template_name = 'home/home.html'
     context_object_name = 'movies'
-    print(Review.objects.order_by('created_on'))
 
     def get_queryset(self):
         queryset = {
             'top_movie': Movie.objects.filter(approved=True).order_by(
                 '-average_stars')[:1],
             'top_rated': Movie.objects.filter(approved=True).order_by(
-                '-average_stars')[:4],
+                '-average_stars')[1:5],
             'new_release': Movie.objects.filter(approved=True).order_by(
                 '-release_date')[:4],
             'new_review': Review.objects.order_by('-created_on')[:4],
